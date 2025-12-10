2a8d1c0faa8875069a777f41f21acd0a33d71be4
Gupples <gupples224@gmail.com>
2025-12-10 14:28:30 -0800

    Refactored count_book_words into a new stats.py file

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from stats import count_book_words
+
 # Takes a filepath as input and returns the contents of the file as a string.
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -5,11 +7,6 @@ def get_book_text(filepath):
         return file_contents
     return None
     
-# Take a book's contents as a string, return the number of words in the string.
-def count_book_words(contents):
-    word_list = contents.split()
-    return len(word_list)
-
 # Use get_book_text with the relative bpath to a file to print the contents of the book to the console.
 def main():
     text = get_book_text("books/frankenstein.txt")
---
+++ stats.py
@@ -0,0 +1,5 @@
+# Take a book's contents as a string, return the number of words in the string.
+def count_book_words(contents):
+    word_list = contents.split()
+    return len(word_list)
+

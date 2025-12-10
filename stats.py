29f114c7815336cb4bb3d8c7182eb444da74c3f5
Gupples <gupples224@gmail.com>
2025-12-10 14:35:19 -0800

    Made function comments clearer

--- stats.py
+++ stats.py
@@ -1,4 +1,11 @@
-# Take a book's contents as a string, return the number of words in the string.
+'''
+COUNT BOOK WORDS
+Splits the contents of a book and returns the number of words it contains
+Inputs:
+    contents - the contents of a book as a string
+Returns:
+    The length of contents.split()
+'''
 def count_book_words(contents):
     word_list = contents.split()
     return len(word_list)

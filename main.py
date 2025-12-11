e1b44ef0c308d746615000b3aa7ee75e9c1a8e0f
Gupples <gupples224@gmail.com>
2025-12-11 11:40:15 -0800

    Enabled dynamic book filepaths via command line

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_book_words, count_book_characters, sort_dictionary
+import sys
 
 # Takes a filepath as input and returns the contents of the file as a string.
 def get_book_text(filepath):
@@ -7,9 +8,15 @@ def get_book_text(filepath):
         return file_contents
     return None
     
-# Use get_book_text with the relative bpath to a file to print the contents of the book to the console.
+# Use get_book_text with the relative path to a file to print the contents of the book to the console.
 def main():
-    filepath = "books/frankenstein.txt"
+
+    # The path to the book should be a part of the executed command.
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    filepath = sys.argv[1]
     text = get_book_text(filepath)
 
     # Begin book report
